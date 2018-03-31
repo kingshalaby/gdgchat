14d8ee3d7bc534444bc8af09e86fc1665e9f05ba
Ahmed Shalaby <kingshalaby@yahoo.com>
2018-03-31 22:43:17 +0200

    adding oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: "messages#index"
-  resources :messages
   
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+
+  root to: "home#show"
 end
